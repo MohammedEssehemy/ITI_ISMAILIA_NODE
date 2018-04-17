d5bec9994ca5368ef9e2449828e101be30762dbe
MohammedEssehemy <mohammedessehemy@gmail.com>
2018-04-17 11:51:02 +0200

    fix type

--- startup/dbInitialize.js
+++ startup/dbInitialize.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 let mongo_url = 'mongodb://localhost:27017/iti_ismailia';
-if(proccess.env.NODE_ENV === 'production'){
+if(process.env.NODE_ENV === 'production'){
   mongo_url = 'mongodb://ashba7_UI:ashba7_UI@ds249269.mlab.com:49269/iti_ismailia'
 }
 mongoose.connect(mongo_url);
